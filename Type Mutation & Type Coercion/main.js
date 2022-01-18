3a8e84b6edcca0efd29748541c3b0827026f3b9d
Nayem <nayemishan377@gmail.com>
2022-01-18 22:43:34 +0600

    Variable type mutation & type coercion added

---
+++ Type Mutation & Type Coercion/main.js
@@ -0,0 +1,16 @@
+//Variable type mutation & type coercion
+
+var myName, job, age, ismarried;
+myName = "Nayem Hossain Ishan";
+job = "Web Designer & Developer";
+age = 21;
+ismarried = false;
+
+console.log("My name is " + name + ". I am a " + job + ". I am " + age + " years old. Am i married? " +ismarried);
+
+ismarried = true;
+alert("Are you " + name + " ? " + ismarried);
+
+var fathers_name = prompt("What is your father name?");
+
+console.log("My name is " + myName + ". My father name is " + fathers_name);
