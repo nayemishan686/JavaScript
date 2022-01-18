0b7bb638a035621afa97e79191895b4e927c83c7
Nayem <nayemishan377@gmail.com>
2022-01-18 20:43:26 +0600

    Basic javascript added

---
+++ JavaScript Basic/main.js
@@ -0,0 +1,30 @@
+// Variable Declare
+
+    /* We can declare varible in two ways
+        1. var 
+        2.let
+        3.const //value will not change
+    */
+   
+   var first = "Nayem";  //value can change and use whole programme
+   console.log(first);
+
+   let second = "Hossain"; //value can change but can't use whole variable
+   console.log(second);
+
+   const PI = 3.1416; //Can't change value
+   console.log(PI);
+
+
+//Undefined variable 
+var cars;
+console.log(cars);
+
+//one statement many variable
+var cycle = "Serious 8ball", price = "12k", color = "Yellow & Black";
+console.log(cycle,price,color);
+
+
+//Re-declaring Variable
+var cycle;
+console.log(cycle);
