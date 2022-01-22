b4e40aa32069c26f62069350da6ace7e72e14555
Nayem <nayemishan377@gmail.com>
2022-01-23 00:27:22 +0600

    array method added

--- array/main.js
+++ array/main.js
@@ -27,3 +27,40 @@ document.getElementById('fifth').innerHTML = car.length;
 
 //accessing last array elements
 document.getElementById('sixth').innerHTML = car[car.length-1]
+
+//array to string
+const fruits = [
+    'Banana',
+    'Orange',
+    'Apple',
+    'Lemon',
+    'Mango'
+];
+document.getElementById('seventh').innerHTML = fruits.toString();
+
+//array pop
+let fruit = fruits.pop();
+document.getElementById('eight').innerHTML = fruits;
+
+//array push
+fruits.push('Mango');
+document.getElementById('eight1').innerHTML = fruits;
+
+//array shift
+let fruit1 = fruits.shift();
+document.getElementById('eight2').innerHTML = fruits;
+
+//array push
+fruits.unshift('Orange');
+document.getElementById('eight3').innerHTML = fruits;
+
+//array delete()
+delete fruits[0];
+document.getElementById('eight4').innerHTML = fruits;
+
+//array Merge
+const arr1 = ["Cecilie", "Lone"];
+const arr2 = ["Emil", "Tobias", "Linus"];
+const arr3 = ["Robin", "Morgan"];
+const myChildren = arr1.concat(arr2,arr3);
+document.getElementById('eight5').innerHTML = myChildren;
