f118b92e15b08b51b76212c9adbe928a5a3be913
Nayem <nayemishan377@gmail.com>
2022-01-24 20:28:43 +0600

    Some problem solved

--- some problem solve/main.js
+++ some problem solve/main.js
@@ -1,34 +1,56 @@
-//object declaration and use only property and value
-const man = {
-    fname : 'Nayem',
-    lname : 'Hossain',
-    age : 22,
-    passion : 'Full stack web Developer',
-    hometown : 'Lakshmipur',
-    mdetails : function() {
-        return 'He is a '+ this.passion;
+//Large numbers between two numbers
+var a = 10;
+var b = 8;
+var c = 15;
+if(a>b){
+    document.getElementById('first').innerHTML = a + ' is greater than ' + b;
+}else{
+    document.getElementById('first').innerHTML = b + ' is greater than ' + a;
+}
+
+//Large numbers between two numbers>b
+if(a>b && a>c){
+    document.getElementById('second').innerHTML = a + ' is greater than ' + b + ' & ' +c;
+}else if(b>a && b>c){
+    document.getElementById('second').innerHTML = b + ' is greater than ' + a + ' & ' +c;
+}else{
+    document.getElementById('second').innerHTML = c + ' is greater than ' + a + ' & ' + b;
+}
+
+//leap year checker
+var year = 2022;
+if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)){
+    document.getElementById('third').innerHTML = year + ' is a leap year.'
+}else{
+    document.getElementById('third').innerHTML = year + ' is not a leap year.'
+}
+
+
+//1-50 even number using for loop
+var even = ' ';
+for(var i = 0; i <= 50; i++){
+    if(i % 2 == 0){
+        even += i + ', '; 
+        document.getElementById('fourth').innerHTML = even;
     }
-};
-//Display data from object
-document.getElementById('first').innerHTML = "My name is " + man.fname +' '+ man.lname + '. I am ' + man.age + ' years old. My dream is '+ man.passion + 'My home town is ' +man.hometown +'.';
+}
 
-
-
-
-//object declaration and use only property and value
-const car = {
-    name : 'BMW',
-    color: 'Black',
-    owner: 'Nayem Hossain',
-    details: function() {
-        return 'This is a '+ this.name + '. It is a ' + this.color +' color. The owner of this car is '+ this.owner + '.';
+//1-50 odd number using for loop
+var even = ' ';
+for(var i = 0; i <= 50; i++){
+    if(i % 2 != 0){
+        even += i + ', '; 
+        document.getElementById('fifth').innerHTML = even;
     }
-};
-//Display data from object
-document.getElementById('second').innerHTML = car.details();
+}
 
-//Access only two Property
-document.getElementById('third').innerHTML = car.name + ' ' + car.color + ' color';
-
-//Access only two Method
-document.getElementById('fourth').innerHTML = car.details() + ' ' + man.mdetails();
+// //if two loop is even it will run
+// for(var i = 0; i <= 10; i++){
+//     if(i % 2 == 0){
+//         for(var j = 0; j<i; j++){
+//             if(i%2 == 0){
+//                 document.getElementById('sixth').innerHTML = i + ' '; 
+//             }
+//         }
+//     }
+// }
